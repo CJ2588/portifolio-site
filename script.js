3b546442affe72fcbec7e996f213ebbb09eca9b8
CJ2588 <chijioken15@gmail.com>
2023-08-14 21:25:47 +0100

    update to js, css & construct html

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 const navigation = document.querySelector(".nav");
-const navToogle= document.querySelector(".imgmenu");
+const navToogle = document.querySelector(".imgmenu");
 const anchorPoints = document.querySelectorAll(".nav a");
 
 //  code for hambuger menu
 
 navToogle.addEventListener("click", () => {
-    const visibility = navigation.getAttribute('data-visible');
-    
-    if(visibility=== "false"){
-        navigation.setAttribute("data-visible", true);
-        navToogle.setAttribute("aria-expanded", true);
-    }
+  const visibility = navigation.getAttribute("data-visible");
 
-    else if (visibility=== "true"){
-        navigation.setAttribute("data-visible", false);
-        navToogle.setAttribute("aria-expanded", false);
-    }
+  if (visibility === "false") {
+    navigation.setAttribute("data-visible", true);
+    navToogle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    navigation.setAttribute("data-visible", false);
+    navToogle.setAttribute("aria-expanded", false);
+  }
 });
 
 //code for smooth scrolling
 
-anchorPoints.forEach(anchorPoint => {
-  anchorPoint.addEventListener("click", function(e) {
+anchorPoints.forEach((anchorPoint) => {
+  anchorPoint.addEventListener("click", function (e) {
     e.preventDefault();
     const targetId = this.getAttribute("href");
     const targetElement = document.querySelector(targetId);
 
     if (targetElement) {
       targetElement.scrollIntoView({
-        behavior: "smooth"
+        behavior: "smooth",
       });
     }
   });
